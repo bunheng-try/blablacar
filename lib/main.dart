7f2b54b1045fdd41d47dc71198c64c5c6be5b5be
Bunheng30 <bunheng0306@gmail.com>
2026-03-19 12:39:43 +0700

    BLA-201 AND BLA-202

--- lib/main.dart
+++ lib/main.dart
@@ -1,52 +1,22 @@
+
 import 'package:flutter/material.dart';
-import 'screens/ride_pref/ride_pref_screen.dart';
-import 'theme/theme.dart';
-import 'screens/screen_widget/app_widget/BlaButton.dart';
+import 'ui/screens/home/home_screen.dart';
+import 'ui/theme/theme.dart';
 
 void main() {
-  runApp(const MyApp());
+   runApp(const BlaBlaApp()); 
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+
+class BlaBlaApp extends StatelessWidget {
+  const BlaBlaApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: appTheme,
-      home:Scaffold(
-        backgroundColor: const Color(0xFFEDEDED),
-        body: SafeArea(
-          child: Column(
-            children: [
-              Expanded(
-                child: RidePrefScreen(),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(16),
-                child: Column(
-                  children: [
-                    Blabutton(
-                      label: "Contact Volodia",
-                      icon: Icons.chat_bubble_outline,
-                      isPrimary: false,
-                      onPressed: () {},
-                    ),
-                    const SizedBox(height: 12),
-                    Blabutton(
-                      label: "Request to book",
-                      icon: Icons.event_seat,
-                      onPressed: () {},
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
-        ),
-      )
+      theme: blaTheme,
+      home: Scaffold(body: HomeScreen()),
     );
   }
 }
-
